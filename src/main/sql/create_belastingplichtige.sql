ea6db5a3dc614f14a065d70b8bc03c2457b0ab55
Mark Prins <markprins@b3partners.nl>
2018-09-10 18:29:16 +0200

    aanpassing belastingplichtige tabel

--- src/main/sql/create_belastingplichtige.sql
+++ src/main/sql/create_belastingplichtige.sql
@@ -1,24 +1,23 @@
 DROP TABLE public.belastingplichtige;
 
 CREATE TABLE public.belastingplichtige (
-  ka_kad_gemeentecode character varying(5), -- Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Kadastrale gemeentecode - Kadastrale gemeentecode
-  ka_sectie character varying(255), -- Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Sectie - Sectie
-  ka_perceelnummer character varying(15), -- Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Perceelnummer - Perceelnummer
-  ka_deelperceelnummer character varying(4), -- Groepsattribuut Kadastrale aanduiding KADASTRAAL PERCEEL.Deelperceelnummer - Deelperceelnummer
-  ka_appartementsindex character varying(4), -- Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Appartementsindex - Appartementsindex
-  kpr_nummer numeric(9,0), -- KPR nummer
-  id bigserial NOT NULL
+  ka_kad_gemeentecode character varying(6),
+  ka_sectie character varying(3),
+  ka_perceelnummer character varying(5),
+  ka_deelperceelnummer character varying(4),
+  ka_appartementsindex character varying(4),
+  kpr_nummer character varying(16),
+  id bigserial NOT NULL,
+  CONSTRAINT belastingplichtige_pkey PRIMARY KEY (id)
 )
 WITH (
   OIDS = FALSE
 );
 
-
-ALTER TABLE public.belastingplichtige ADD PRIMARY KEY (id);
-
+COMMENT ON TABLE public.belastingplichtige IS 'Belastingplichtigen uit GIBS';
 COMMENT ON COLUMN public.belastingplichtige.ka_kad_gemeentecode IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Kadastrale gemeentecode - Kadastrale gemeentecode';
-COMMENT ON COLUMN public.belastingplichtige.ka_sectie IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Sectie - Sectie';
-COMMENT ON COLUMN public.belastingplichtige.ka_perceelnummer IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Perceelnummer - Perceelnummer';
-COMMENT ON COLUMN public.belastingplichtige.ka_deelperceelnummer IS 'Groepsattribuut Kadastrale aanduiding KADASTRAAL PERCEEL.Deelperceelnummer - Deelperceelnummer';
-COMMENT ON COLUMN public.belastingplichtige.ka_appartementsindex IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Appartementsindex - Appartementsindex';
-COMMENT ON COLUMN public.belastingplichtige.kpr_nummer IS 'KPR nummer';
+COMMENT ON COLUMN public.belastingplichtige.ka_sectie IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Sectie - Sectie met voorloop nullen';
+COMMENT ON COLUMN public.belastingplichtige.ka_perceelnummer IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Perceelnummer - Perceelnummer met voorloop nullen';
+COMMENT ON COLUMN public.belastingplichtige.ka_deelperceelnummer IS 'Groepsattribuut Kadastrale aanduiding KADASTRAAL PERCEEL.Deelperceelnummer - Deelperceelnummer met voorloop nullen';
+COMMENT ON COLUMN public.belastingplichtige.ka_appartementsindex IS 'Groepsattribuut Kadastrale aanduiding APPARTEMENTSRECHT.Appartementsindex - Appartementsindex met voorloop nullen';
+COMMENT ON COLUMN public.belastingplichtige.kpr_nummer IS 'KPR nummer uit GIBS';
---
+++ src/main/sql/testdata.sql
@@ -0,0 +1,3 @@
+-- test data
+insert into belastingplichtige values('EDE02','A','02772','P','0000','1234567');
+insert into belastingplichtige values('EDE02','A','03314','P','0000','2345678');
